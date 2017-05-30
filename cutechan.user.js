2754a67f5c38e5fd9c0730804bed5590c0840640
Kagami Hiiragi <kagami@genshiken.org>
2017-05-30 11:16:07 +0300

    0.3.8

--- cutechan.user.js
+++ cutechan.user.js
@@ -6,7 +6,7 @@
 // @updateURL   https://raw.githubusercontent.com/Kagami/cutechan/master/cutechan.user.js
 // @include     https://0chan.hk/*
 // @include     http://nullchan7msxi257.onion/*
-// @version     0.3.7
+// @version     0.3.8
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setClipboard
